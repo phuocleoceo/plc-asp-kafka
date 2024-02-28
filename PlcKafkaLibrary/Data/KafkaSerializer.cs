81df84bebe032f23908245314df076924e80e2cb
phuocleoceo <ht10082001@gmail.com>
2024-02-28 23:30:06 +0700

    Kafka Library

---
+++ PlcKafkaLibrary/Data/KafkaSerializer.cs
@@ -0,0 +1,24 @@
+using Confluent.Kafka;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace PlcKafkaLibrary.Data;
+
+internal sealed class KafkaSerializer<T> : ISerializer<T>
+{
+    public byte[] Serialize(T data, SerializationContext context)
+    {
+        if (typeof(T) == typeof(Null))
+        {
+            return null;
+        }
+
+        if (typeof(T) == typeof(Ignore))
+        {
+            throw new NotSupportedException("Not Supported.");
+        }
+
+        string json = JsonConvert.SerializeObject(data);
+        return Encoding.UTF8.GetBytes(json);
+    }
+}
